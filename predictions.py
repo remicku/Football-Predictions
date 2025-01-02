8807e745ca917ca4b125dc6edca00f1bd4a29491
remi.brenaut <remi.brenaut@epita.fr>
2025-01-02 12:02:46 +0100

    feat: predictions

--- predictions.py
+++ predictions.py
@@ -21,3 +21,48 @@ preds = rf.predict(test[predictors])
 
 acc = accuracy_score(test["target"], preds)
 combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
+
+grouped_matches = matches.groupby("team")
+group = grouped_matches.get_group("Manchester City")
+
+def rolling_averages(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(10, closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+
+matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x, cols, new_cols))
+matches_rolling = matches_rolling.droplevel("team")
+matches_rolling.index = range(matches_rolling.shape[0])
+
+def make_predictions(data, predictors):
+    train = data[data["date"] < '2022-01-01']
+    test = data[data["date"] > '2022-01-01']
+    rf.fit(train[predictors], train["target"])
+    preds = rf.predict(test[predictors])
+    combined = pd.DataFrame(dict(actual=test["target"], prediction=preds), index=test.index)
+    precision = precision_score(test["target"], preds)
+    return combined, precision
+
+combined, precision = make_predictions(matches_rolling, predictors + new_cols)
+combined = combined.merge(matches_rolling[["date", "team", "opponent", "result"]], left_index=True, right_index=True)
+
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
+
+map_values = {
+    "Brighton and Hove Albion": "Brighton",
+    "Manchester United": "Manchester Utd",
+    "Newcastle United": "Newcastle Utd",
+    "Tottenham Hotspur": "Tottenham",
+    "West Ham United": "West Ham",
+    "Wolverhampton Wanderers": "Wolves"
+}
+
+mapping = MissingDict(**map_values)
+combined["new_team"] = combined["team"].map(mapping)
+merged = combined.merge(combined, left_on=["date", "new_team"], right_on=["date", "opponent"])
